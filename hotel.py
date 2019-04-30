cb33507344ac794c3d57b8f0cf07cbfd5aec1647
KsushaShlapakova <shlapakova2000@mail.ru>
2019-04-30 22:31:24 +0700

    Final commit

--- hotel.py
+++ hotel.py
@@ -1,13 +1,11 @@
-import datetime
+# Developers: Shlapakova 90%, Bateneva 60%, Zikova 40%.
+
 from dateutil.relativedelta import relativedelta
+import datetime
 import random
 
 
 class Room:
-    food = {'без питания': 0.00, 'завтрак': 280.00, 'полупансион': 1000.00}
-    price_ = {'одноместный': 2900.00, 'двухместный': 2300.00, 'люкс': 4100.00, 'полулюкс': 3200.00}
-    grow_price = {'стандарт': 1.0, 'стандарт_улучшенный': 1.2, 'апартамент': 1.5}
-
     def __init__(self, text):
         self.text = text
         i = text.rstrip('\n')
@@ -24,27 +22,31 @@ class Room:
     def __str__(self):
         return '%s.%s.%s' % (str(self.date_year), str(self.date_month), str(self.date_day))
 
-    def price(self, budget, discount):
-        food = {'без питания': 0., 'завтрак': 280., 'полупансион': 1000.}
-        price_ = {'одноместный': 2900., 'двухместный': 2300., 'люкс': 4100., 'полулюкс': 3200.}
-        grow_price = {'стандарт': 1., 'стандарт_улучшенный': 1.2, 'апартамент': 1.5}
+    def __price(self, budget, discount):
+        food = {'без питания': 0.00, 'завтрак': 280.00, 'полупансион': 1000.00}
+        price_ = {'одноместный': 2900.00, 'двухместный': 2300.00, 'люкс': 4100.00, 'полулюкс': 3200.00}
+        grow_price = {'стандарт': 1.0, 'стандарт_улучшенный': 1.2, 'апартамент': 1.5}
 
-        p = price_[self.type_] * grow_price[self.comfort] * discount
+        pay = price_[self.type_] * grow_price[self.comfort] * discount
 
-        if p > budget:
+        if pay > budget:
             return -1
 
-        n = budget - p
+        expenses = budget - pay
         self.food_price = 0
         self.food_type = list(food.keys())[list(food.values()).index(0.)]
 
         for i in food:
-            if n >= food[i]:
+            if expenses >= food[i]:
                 self.food_price = food[i]
                 self.food_type = i
-        p += self.food_price
+        pay += self.food_price
 
-        return p  # макс цена по бюджету клиента
+        return pay
+
+    @property
+    def price(self):
+        return self.__price
 
     def reserve(self, date, days):
         date = date.split('.')
@@ -53,53 +55,62 @@ class Room:
         self.date_month = int(date[1])
         self.date_day = int(date[0])
 
-        d = datetime.date(self.date_year, self.date_month, self.date_day) + relativedelta(days=+(days-1))
+        entry = datetime.date(self.date_year, self.date_month, self.date_day) + relativedelta(days=+(days-1))
 
-        a = str(d).split('-')
+        a = str(entry).split('-')
 
         self.date_year = int(a[0])
         self.date_month = int(a[1])
         self.date_day = int(a[2])
 
     def check_reserve(self, date):
+        arrival = date.split('.')
+        date_year = int(arrival[2])
+        date_month = int(arrival[1])
+        date_day = int(arrival[0])
 
-        d = date.split('.')
-        date_year = int(d[2])
-        date_month = int(d[1])
-        date_day = int(d[0])
-
-        if self.date_year == None:
+        if self.date_year is None:
             self.reserve(date, 0)
 
-        d1 = datetime.date(date_year, date_month, date_day)  # дата бронирования
-        d2 = datetime.date(self.date_year, self.date_month, self.date_day)  # дата освобождения
+        reservation = datetime.date(date_year, date_month, date_day)
+        exit_ = datetime.date(self.date_year, self.date_month, self.date_day)
 
-        if d1 >= d2:
-            return True  # номер свободен
+        if reservation >= exit_:
+            return True
         return False
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class Hotel:
     def __init__(self, text):
         self.text = text
         self.room_with_price = []
-        self.rooms = [Room(i.rstrip('\n')) for i in text]  # список всех комнат
-        self.max_people = max([x.person for x in self.rooms])  # спиок вмещаемости каждой комнаты
+        self.rooms = [Room(i.rstrip('\n')) for i in text]
+        self.max_people = max([x.person for x in self.rooms])
         self.profit = 0
         self.lose = 0
 
-        self.ones = len([x for x in self.rooms if x.type_ == 'одноместный'])
-        self.twos = len([x for x in self.rooms if x.type_ == 'двухместный'])
+        self.ones = len([x for x in self.rooms if x.type_ == 'одноместный'])
+        self.twos = len([x for x in self.rooms if x.type_ == 'двухместный'])
         self.lux = len([x for x in self.rooms if x.type_ == 'люкс'])
         self.half_lux = len([x for x in self.rooms if x.type_ == 'полулюкс'])
 
+    def __str__(self):
+        info = ''
+        info += '\nНомера нашего отеля:\n'
+        for i in self.text:
+            info += i
+        return info
+
     @staticmethod
     def agree():
-        a = random.randint(1, 100)
-        if a >= 25:
-            return True  # 'Клиент согласен. Номер забронирован.\n'
+        good = random.randint(1, 100)
+        if good >= 25:
+            return True
         else:
-            return False  # 'Клиент отказался от варианта.\n'
+            return False
 
     def find_room(self, people, date, budget):
         for p in range(people, self.max_people + 1):
@@ -114,54 +125,42 @@ class Hotel:
                 discount = 1.
                 if p > people:
                     discount = .7
-                m = i.price(budget, discount)
+                mood = i.price(budget, discount)
 
-                if m == -1:
+                if mood == -1:
                     continue
 
-                rooms.append((m, i))
+                rooms.append((mood, i))
             rooms.sort(key=lambda tup: tup[0])
             if rooms:
                 return rooms[-1]
         return None
 
     def bookings(self, file='booking.txt'):
-
-        with open('booking.txt', encoding='utf8') as f:
+        with open(file, encoding='utf8') as f:
             text = f.readlines()
             current_date = ''
 
             for i, k in enumerate(text):
-                j = k.split()  # 01.03.2018 Аксёнов Лаврентий Семенович 3 03.03.2018 2 4600
-                date_reserve = j[0]
-                name = j[1] + ' ' + j[2] + ' ' + j[3]
-                amount_people = int(j[4])
-                date_in = j[5]
-                days = int(j[6])
-                budget = int(j[7])
+                fio = k.split()
+                date_reserve = fio[0]
+                amount_people = int(fio[4])
+                date_in = fio[5]
+                days = int(fio[6])
+                budget = int(fio[7])
 
-                if not current_date:
-                    current_date = date_reserve
-
-                if current_date != date_reserve or i == (len(text) - 1):
-                    print('='*50, '\nИтог за', current_date, '\n')
-                    self.print_status(current_date)
-                    self.profit = 0
-                    self.lose = 0
-                    current_date = date_reserve
-
-                print('-'*50, '\nПоступила заявка на бронирование:\n', k)
+                print('-'*105, '\nПоступила заявка на бронирование:\n', k)
 
                 ret = self.find_room(amount_people, date_in, budget)
                 if ret:
-                    r = ret[1]
+                    right = ret[1]
                     current_price = ret[0] * amount_people
                     print('Найден:\n')
-                    print('номер', r.number, r.type_, r.comfort, 'рассчитан на',
-                          r.person, 'чел. фактически', amount_people, 'чел.',
-                          r.food_type, 'стоимость', current_price, 'руб./сутки\n')
+                    print('номер', right.number, right.type_, right.comfort, 'рассчитан на',
+                          right.person, 'чел. фактически', amount_people, 'чел.',
+                          right.food_type, 'стоимость', float("{0:.1f}".format(current_price)), 'руб./сутки\n')
                     if self.agree():
-                        r.reserve(date_in, days)
+                        right.reserve(date_in, days)
                         self.profit += current_price
                         print('Клиент согласен. Номер забронирован.\n')
                     else:
@@ -171,28 +170,42 @@ class Hotel:
                     self.lose += (budget * amount_people)
                     print('Предложений по данному запросу нет. В бронировании отказано.\n')
 
+                if not current_date:
+                    current_date = date_reserve
+
+                if current_date != date_reserve or i == (len(text) - 1):
+                    print('=' * 40, '\nИтог за', current_date, '\n')
+                    self.print_status(current_date)
+                    self.profit = 0
+                    self.lose = 0
+                    current_date = date_reserve
+
     def reserved_rooms(self, date):
-        r = []
+        ring = []
         for i in self.rooms:
             if not i.check_reserve(date):
-                r.append(i)
-        return r
+                ring.append(i)
+        return ring
 
     def print_status(self, date):
-        r = self.reserved_rooms(date)
-        o = len([x for x in r if x.type_ == 'одноместный'])
-        d = len([x for x in r if x.type_ == 'двухместный'])
-        p = len([x for x in r if x.type_ == 'полулюкс'])
-        l = len([x for x in r if x.type_ == 'люкс'])
-        percent = len(r) * 100 / len(self.rooms)
+        stockpile = self.reserved_rooms(date)
+        one = len([x for x in stockpile if x.type_ == 'одноместный'])
+        two = len([x for x in stockpile if x.type_ == 'двухместный'])
+        plux = len([x for x in stockpile if x.type_ == 'полулюкс'])
+        lux = len([x for x in stockpile if x.type_ == 'люкс'])
+        percent = float("{0:.1f}".format(len(stockpile) * 100 / len(self.rooms)))
 
-        print('Количество занятых номеров:', len(r), '\n')
-        print('Количество свободных номеров:', len(self.rooms) - len(r), '\n')
+        print('Количество занятых номеров за всё время:', len(stockpile))
+        print('Количество свободных номеров:', len(self.rooms) - len(stockpile), '\n')
         print('Занятость по категориям:\n')
-        print('Одноместных:', o, 'из', self.ones)
-        print('Двухместных:', d, 'из', self.twos)
-        print('Полулюкс:', p, 'из', self.half_lux)
-        print('Люкс:', l, 'из', self.lux)
+        print('Одноместных:', one, 'из', self.ones)
+        print('Двухместных:', two, 'из', self.twos)
+        print('Полулюкс:', plux, 'из', self.half_lux)
+        print('Люкс:', lux, 'из', self.lux)
         print('Процент загруженности гостиницы:', percent, '%')
         print('Доход за день:', self.profit, 'руб.')
         print('Упущенный доход: ', self.lose, 'руб.\n')
+        print('=' * 40)
+
+    def __repr__(self):
+        return self.__str__()
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from hotel import *
 with open('fund.txt', encoding='utf8') as f:
     text = f.readlines()
     hotel = Hotel(text)
-    a = hotel.bookings()
-    print(a)
+    model = hotel.bookings()
+    print(model)
